4a6df9920d8cc3c9e1acccbc78537df111e3a4ed
Muhammad Umar <mu8494759@gmail.com>
2024-06-30 15:04:08 +0500

    Remove Element

---
+++ Remove_Element.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/remove-element/
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums[i:i+1] = [] #delete the matching values
+            else:
+                i += 1
+        return len(nums)
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
