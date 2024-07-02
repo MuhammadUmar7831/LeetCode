2ac67ea015da721c4aade1da492633cb005cf68b
Muhammad Umar <mu8494759@gmail.com>
2024-07-02 10:18:54 +0500

    Length of Argument Passed

---
+++ Length_of_Arguments_Passed.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/return-length-of-arguments-passed/
+
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+//shortcut
+var argumentsLength = (...args) => args.length
