189e206825dd50f864aeeda4d34a9bc196d71e9d
Muhammad Umar <mu8494759@gmail.com>
2024-07-02 10:24:51 +0500

    Score of a String

---
+++ Score_of_a_String.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/score-of-a-string/
+
+def abs(x):
+    return -x if x < 0 else x
+class Solution(object):
+    def scoreOfString(self, s):
+        result = 0
+        for i in range(len(s)-1):
+            result += abs(ord(s[i])-ord(s[i+1]))
+        return result
+        
