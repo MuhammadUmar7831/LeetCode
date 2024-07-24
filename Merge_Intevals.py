07e338558065e434818bc24f985e4f5e24024ea8
MuhammadUmar7831 <mu8494759@gmail.com>
2024-07-24 14:49:51 +0500

    Merge Intervals

---
+++ Merge_Intevals.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/merge-intervals/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        i = 0
+        while i < len(intervals) - 1:
+            if intervals[i][1] >= intervals[i+1][0]:
+                intervals[i+1][0] = intervals[i][0]
+                intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1])
+                intervals.pop(i)
+            else:
+                i += 1
+            
+        return intervals
+        
